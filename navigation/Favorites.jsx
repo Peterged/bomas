e3e7f0bddf1962e393eb501def982a56516ebfb6
Guardian <nexelhub@gmail.com>
2023-10-08 14:42:17 +0800

    navigation setup

---
+++ navigation/Favorites.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import {
+    StyleSheet,
+    TouchableOpacity,
+    Text,
+    View,
+    ImageBackground,
+    Image,
+    Alert,
+    Dimensions
+} from 'react-native';
+
+import { StatusBar } from 'expo-status-bar';
+import { useFonts } from 'expo-font';
+import { Color } from '~/assets/styles/Style.js';
+import * as FileSystem from 'expo-file-system';
+import { NavigationContainer } from '@react-navigation/native';
+
+// Create a custom header on top of page
+export default function Favorites ({ navigation }) {
+    return (
+        <NavigationContainer>
+            <View style={header}></View>
+
+            <StatusBar style='auto' />
+        </NavigationContainer>
+    )
+}
+
+
+const styles = StyleSheet({
+    container: {
+        flex: 1,
+    },
+    header: {
+
+    }
+})
